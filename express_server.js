12478ade83f48a6a5d35a3859d2a429f60210087
Rebecca <rebecca90469@gmail.com>
2024-02-24 08:55:22 -0500

    Create new page for data related to web page info for tiny url ID

--- express_server.js
+++ express_server.js
@@ -22,6 +22,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const templateVars = { id: id, longURL: urlDatabase[id] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body> <b>World</></body></html>\n");
 });
