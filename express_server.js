e7d4c6fbd9810aa601748fd598c8e930a80836e6
Rebecca <rebecca90469@gmail.com>
2024-03-01 11:47:35 -0500

    Add chai-http

--- express_server.js
+++ express_server.js
@@ -10,6 +10,8 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.json());
+
 app.use(cookieSession({ 
   name: "session",
   secret: "secretCookieKey"
